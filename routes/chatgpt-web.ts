beb6390178b422ff1832caad77151e9dc9424d63
SmileSmith <longde_chen@163.com>
2023-05-12 20:40:32 +0800

    fix: 透传接口conversationId

--- routes/chatgpt-web.ts
+++ routes/chatgpt-web.ts
@@ -56,7 +56,7 @@ router.post('/verify', async (req, res) => {
 router.post('/chat-process', async (req, res) => {
   const params = { ...req.body, ...req.query } as any;
   const { prompt: message, options = {} } = params;
-  const { parentMessageId } = options;
+  const { parentMessageId, conversationId } = options;
 
   try {
     if (!message) throw new Error('请传入prompt参数');
@@ -75,7 +75,7 @@ router.post('/chat-process', async (req, res) => {
     const chatGPTResponse = await handleChatGPT(
       req,
       res,
-      { message, parentMessageId, conversationId: '' },
+      { message, parentMessageId, conversationId },
       (processResponse: ChatMessage) => {
         logger.debug(processResponse);
         res.write(
