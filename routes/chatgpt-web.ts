254a069b640fff1607ca0453ad41c6f3c9d057e9
SmileSmith <longde_chen@163.com>
2023-05-12 19:55:01 +0800

    feat: 支持chatgpt-web proxy模式

---
+++ routes/chatgpt-web.ts
@@ -0,0 +1,60 @@
+import express from 'express';
+import Logger from 'log4js';
+import { ChatMessage } from 'chatgpt';
+import * as dotenv from 'dotenv';
+import { handleChatGPT } from './api';
+
+dotenv.config();
+
+const router = express.Router();
+const logger = Logger.getLogger('chatgpt');
+logger.level = process.env.LOG_LEVEL || 'debug';
+
+router.post('/session', async (req, res) => {
+  res.send({
+    status: 'Success',
+    message: '',
+    data: { auth: true, model: 'ChatGPTUnofficialProxyAPI' },
+  });
+});
+
+router.post('/chat-process', async (req, res) => {
+  const params = { ...req.body, ...req.query } as any;
+  const { prompt: message, options = {} } = params;
+  const { parentMessageId } = options;
+
+  try {
+    if (!message) throw new Error('请传入prompt参数');
+
+    // 1. 设置响应头
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'authorization, Content-Type'
+    );
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Content-type', 'application/octet-stream');
+
+    let firstChunk = true;
+
+    const chatGPTResponse = await handleChatGPT(
+      req,
+      res,
+      { message, parentMessageId, conversationId: '' },
+      (processResponse: ChatMessage) => {
+        logger.debug(processResponse);
+        res.write(
+          firstChunk
+            ? JSON.stringify(processResponse)
+            : `\n${JSON.stringify(processResponse)}`
+        );
+        firstChunk = false;
+      }
+    );
+    res.write(JSON.stringify(chatGPTResponse));
+  } catch (error: unknown) {
+    res.write(JSON.stringify(error));
+  } finally {
+    res.end();
+  }
+});
